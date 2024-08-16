428db8de9c22a93104c5eb7a53bc191e6fc13196
niazimurataj <niazi@murataj.com>
2024-08-16 13:21:33 -0700

    Trololo initialtski commitski kompromat

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//SCREW IT: PRODUCT PMR COPILOT
+// NAME: 
+// TAKE YOUR SBA TEMPLATES AND MAKE THEM INTERACTIVE RECORD KEEPING FOR 
+// PRODUCT DEVELOPMENT EFFORTS
+// Add in assistant that automatically finds people for you to interview
+// --- lists emails and such, even writes them--at one click
+// Even AI agent that gives design/inspiration ideas (and creates Figma)
+// AI AGENT THAT DOES YOUR PRODUCT FINANCE
+// AND TOUR CHANNEL VALIDATION AND ANALYSIS
+
+// OLD IDEA
+//set up a simple iOS time tracker and todo list
+// goal: easily (even by voice) record what you're doing at a given moment
+// framework: KPIs and OKRs related to goals
+// purpose: keeps you engaged with your goals and helps steer your behavior
+// widgets: in iOS, figure out how to have some data on widgets
+// feature: should have a smart todo and should have calendar that uses LLM to populate calendar entries
+// APIs: GPT-4o API, Google Calendar API
+// Database: AWS database
+// Deployment: AWS EC2 or some other
+// DevTools: great to get familiar with Jenkins, git, etc.
+// Jupyter: maybe host one and use a webhook to get it to perform something? maybe AWS lambda?
+// Learn: what are webhooks? how to tie together above concepts?
+
+// Desiderata: I want this thing to keep in check to achieve my goals
---
+++ planner_chat_ui.js
@@ -0,0 +1,10 @@
+//TODO: chat text window that converts your text to calendar plan intelligently
+// TODO: design screens 
+/*
+1. scrollable chat dialogue
+2. text entry form field
+3. send button
+4. clarification screen
+*/
+
+// QUESTION: can I embed a google calendar into my own app?
